97e45534614361ca5991121227b3ee6dbb03525a
Xin Wang <wangamanda.lu@gmail.com>
2014-11-30 14:23:06 -0500

    first commit

---
+++ chap1/Problem4.java
@@ -0,0 +1,30 @@
+public class Problem4 {
+	public static void main(String[] args) {
+		String s = "Mr John Smith    ";
+		replaceSpaces(s.toCharArray(), 13);
+	}
+
+	public static void replaceSpaces(char[] str, int length) {
+		int cnt = 0;
+		for(int i = 0 ; i < length ; i ++ ) {
+			if(str[i] == ' ') {
+				cnt++;
+			}
+		}
+		int newLength = length+2*cnt;
+		int i = 0 ; 
+		int j = 0 ;
+		char[] newArr = new char[newLength];
+		while(i < length) {
+			if(str[i] != ' ') {
+				newArr[j++] = str[i++];
+			} else {
+				newArr[j++] = '%';
+				newArr[j++] = '2';
+				newArr[j++] = '0';
+				i++;
+			}
+		}
+		System.out.println(new String(newArr));
+	}
+}
