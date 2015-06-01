14428fd4d9735d69c2bcc3bdfa8946f59ea8c46f
Xin Wang <wangamanda.lu@gmail.com>
2015-05-31 22:54:28 -0400

    problem 3

--- chap1/Problem8.java
+++ chap1/Problem8.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Problem3 {
 	public static void main(String[] args) {
 		if (isPermutation(args[0], args[1])) {
@@ -13,20 +11,17 @@ public class Problem3 {
 		if (s1 == null) {
 			return s2 == null;
 		}
-
 		if (s2 == null) {
 			return s1 == null;
 		}
-		
 		if (s1.length() != s2.length()) {
 			return false;
 		}
-		return (sort(s1).equals(sort(s2)));
-	}
-
-	public static String sort(String s) {
-		char[] str = s.toCharArray();
-		Arrays.sort(str);
-		return String.valueOf(str);
+		String s = s2+s2;
+		if (s.contains(s1)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
